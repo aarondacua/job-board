4960b6f4db241d94604c28e8f82d3233b17d15fc
aarondacua <138538944+aarondacua@users.noreply.github.com>
2024-01-07 10:05:53 +0800

    Search Bar and loading state of filter button

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import H1 from "@/components/ui/h1";
+
 export default function error() {
-  return <div>error</div>;
+  return (
+    <main className="m-auto max-w-5xl px-3 text-center my-10">
+      <H1>Error</H1>
+      <p>An unexpected error occured</p>
+    </main>
+  );
 }
